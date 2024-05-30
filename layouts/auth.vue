1214cab9bb565a2893c230260959f036ab395f7f
AnttoLion <anttolion@gmail.com>
2024-05-29 21:01:47 -0400

    auth layout minor change

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -9,15 +9,6 @@ useHead({
 <template>
   <div class="h-screen flex items-center justify-center overlay">
     <div class="gradient" />
-
-    <UButton
-      icon="i-heroicons-home"
-      label="Home"
-      to="/"
-      color="black"
-      class="absolute top-4"
-    />
-
     <slot />
   </div>
 </template>
